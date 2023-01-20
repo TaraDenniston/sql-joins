2eb6866fa3f0076057e642728c30afc218611461
Tara Denniston <taradenniston@gmail.com>
2023-01-20 15:53:40 -0500

    complete exercises

--- queries.sql
+++ queries.sql
@@ -1,1 +1,34 @@
--- write your queries here
+-- SQL Queries for Joins Exercises - Subunit 22.3: SQL Relationships
+
+
+-- Query 1: Join the two tables so that every column and record appears, 
+-- regardless of if there is not an owner_id.
+SELECT * 
+FROM owners o
+FULL JOIN vehicles v
+ON v.owner_id = o.id;
+
+-- Query 2: Count the number of cars for each owner. Display the owners 
+-- first_name, last_name and count of vehicles. The first_name should be 
+-- ordered in ascending order.
+SELECT o.first_name, o.last_name, COUNT(v.owner_id)
+FROM owners o
+JOIN vehicles v
+ON v.owner_id = o.id
+GROUP BY (o.first_name, o.last_name)
+ORDER BY o.first_name;
+
+-- Query 3: Count the number of cars for each owner and display the average 
+-- price for each of the cars as integers. Display the owners first_name, 
+-- last_name, average price and count of vehicles. The first_name should be 
+-- ordered in descending order. Only display results with more than one 
+-- vehicle and an average price greater than 10000.
+SELECT o.first_name, o.last_name, ROUND(AVG(v.price)), COUNT(v.owner_id)
+FROM owners o
+JOIN vehicles v
+ON v.owner_id = o.id
+GROUP BY (o.first_name, o.last_name)
+HAVING COUNT(v.owner_id) > 1 AND AVG(v.price) > 10000
+ORDER BY o.first_name DESC;
+
+
